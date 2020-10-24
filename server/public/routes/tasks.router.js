baf76fcdd101ce5ab01888aa1244c362d5de09bc
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-10-24 11:59:55 -0500

    post and get route created. css file sourced

--- server/public/routes/tasks.router.js
+++ server/public/routes/tasks.router.js
@@ -12,4 +12,33 @@ const pool = new Pool({
   idleTimeoutMillis: 10000,
 });
 
+router.get('/', (req, res) => {
+  const queryTasks = `SELECT * FROM "Tasks";`;
+  pool
+    .query(queryTasks)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log(`Error in GET /tasks ${error}`);
+      res.sendStatus(500);
+    });
+});
+
+router.post('/', (req, res) => {
+  console.log(`In /tasks with`, req.body);
+  const taskToAdd = req.body;
+  const taskQuery = `INSERT INTO "Tasks" ("task" , "notes" , "complete") VALUES ($1, $2, $3);`;
+  pool
+    .query(taskQuery, [taskToAdd.task, taskToAdd.notes, taskToAdd.complete])
+    .then((responseFromDatabase) => {
+      console.log(responseFromDatabase);
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(`Error in POST /tasks ${error}`);
+      res.sendStatus(500);
+    });
+});
+
 module.exports = router;
